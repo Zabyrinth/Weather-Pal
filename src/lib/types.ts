32591ae3b6ad9a480232012d8ad1728651240b97
Mahir Ashab <ashabmahir283@gmail.com>
2025-06-30 16:13:39 +0000

    i replaced the api key now check

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -38,7 +38,6 @@ export interface ForecastData {
       feels_like: number;
       temp_min: number;
       temp_max: number;
-      temp_max: number;
       pressure: number;
       sea_level: number;
       grnd_level: number;
